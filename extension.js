e949005b45ff1673555e85ae9df56e24aa0d9e38
Mike <mike@recut.com>
2023-11-29 18:17:10 -0800

    Fixes #2

--- extension.js
+++ extension.js
@@ -11,15 +11,36 @@ const activate = ({ subscriptions }) => {
     const currentValue = config.get(configKey)
     const newValue = currentValue ? undefined : true
     config.update(configKey, newValue, true)
+    setContext(newValue)
   })
 
   let hideCommand = registerCommand(`${namespace}.hide`, () => {
     getConfiguration('explorer').update(configKey, true, true)
+    setContext(true)
   })
 
   let showCommand = registerCommand(`${namespace}.show`, () => {
     getConfiguration('explorer').update(configKey, undefined, true)
+    setContext(undefined)
   })
+
+  let setContext = (state) => { 
+    vscode.commands.executeCommand('setContext', `${namespace}.toggled`, state)
+  }
+
+  const config = getConfiguration('explorer')
+  setContext(config.get(configKey) || undefined);
+
+  subscriptions.push(vscode.Disposable.from(
+      vscode.workspace.onDidChangeConfiguration((e) => {
+        if (e.affectsConfiguration(`explorer.${configKey}`)) {
+          const config = getConfiguration('explorer')
+          setContext(config.get(configKey) || undefined);
+        }
+      })
+    )
+  )
+
 
   subscriptions.push(toggleCommand)
   subscriptions.push(hideCommand)
