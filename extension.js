e4469f7e6c29d63d1d56a1f80c7159602b82717a
mikeburgh <mike@recut.com>
2024-09-05 21:28:50 -0700

    support workspace settings; add explorer icon (#3)
    * Fixes #2
    
    * Fixes #1
    
    * use local copy of `value` parameter
    
    ---------
    
    Co-authored-by: Tim G. Thomas <github@timgthomas.com>

--- extension.js
+++ extension.js
@@ -6,20 +6,47 @@ const configKey = 'excludeGitIgnore'
 const namespace = 'explorer-gitignore'
 
 const activate = ({ subscriptions }) => {
-  let toggleCommand = registerCommand(`${namespace}.toggle`, () => {
+  let toggleCommand = registerCommand(`${namespace}.toggle`, (value) => {
     const config = getConfiguration('explorer')
-    const currentValue = config.get(configKey)
-    const newValue = currentValue ? undefined : true
-    config.update(configKey, newValue, true)
+    const isGlobal = config.inspect(configKey).workspaceValue === undefined
+    let newValue = value
+
+    if (newValue === undefined) {
+      newValue = config.get(configKey) ? false : true  
+    }
+
+    if (!newValue && isGlobal) { 
+      // Use undefined only global setting, otherwise it removes the setting and it won't restore to the workspace when toggled back
+      newValue = undefined
+    }
+
+    config.update(configKey, newValue, isGlobal)
+    setContext(newValue)
   })
 
   let hideCommand = registerCommand(`${namespace}.hide`, () => {
-    getConfiguration('explorer').update(configKey, true, true)
+    vscode.commands.executeCommand(`${namespace}.toggle`,  true)
   })
 
   let showCommand = registerCommand(`${namespace}.show`, () => {
-    getConfiguration('explorer').update(configKey, undefined, true)
+    vscode.commands.executeCommand(`${namespace}.toggle`,  false)
   })
+
+  let setContext = (state) => { 
+    vscode.commands.executeCommand('setContext', `${namespace}.hidden`, state)
+  }
+
+  const config = getConfiguration('explorer')
+  setContext(config.get(configKey));
+
+  subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration((e) => {
+      if (e.affectsConfiguration(`explorer.${configKey}`)) {
+        const config = getConfiguration('explorer')
+        setContext(config.get(configKey));
+      }
+    })
+  )
 
   subscriptions.push(toggleCommand)
   subscriptions.push(hideCommand)
